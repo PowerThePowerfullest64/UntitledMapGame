64e1228838e1157f62d3459f3cba07d61d5f3f94
Ducc08 <andersmb2008@gmail.com>
2026-04-28 17:00:15 +0200

    Added Camera2D with Controls

--- observer_camera.gd
+++ observer_camera.gd
@@ -1,11 +1,26 @@
 extends Camera2D
 
 @export var speed: float = 750.0
+@export var zoom_sensitivity: float = 1.0
+@export var zoom_rate: float = 10.0
+
+var target_zoom: Vector2
+
+func _ready() -> void:
+	target_zoom = zoom
 
 func _process(delta: float) -> void:
 	update_movement(delta)
+	update_zoom()
+	zoom = zoom.lerp(target_zoom, zoom_rate * delta)
 
 func update_movement(delta: float) -> void:
 	var move_dir: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
-	position += move_dir * speed * delta
+	position += move_dir * speed / zoom.x * delta
+
+func update_zoom() -> void:
+	if Input.is_action_just_pressed("zoom_in"):
+		target_zoom *= 1.0 + (0.1 * zoom_sensitivity)
+	elif Input.is_action_just_pressed("zoom_out"):
+		target_zoom /= 1.0 + (0.1 * zoom_sensitivity)
