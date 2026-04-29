c8be8780c363b1cb82ea69e05a9914679dd8514e
Ducc08 <andersmb2008@gmail.com>
2026-04-29 20:16:39 +0200

    Added Nation Cell Adding/Subtracting

--- nation.gd
+++ nation.gd
@@ -3,9 +3,26 @@ class_name Nation extends Object
 var id: int
 var population: float = 0.0
 var cells: Array[int] = [] # the cells which it owns stored as ids
+var area: int = 0
 
 func _init(_id: int) -> void:
 	id = _id
+
+func add_cell(cell_id: int) -> bool:
+	if cells.has(cell_id): return false
+	
+	cells.append(cell_id)
+	area += 1
+	
+	return true
+
+func remove_cell(cell_id: int) -> bool:
+	if not cells.has(cell_id): return false
+	
+	cells.erase(cell_id)
+	area -= 1
+	
+	return true
 
 func update() -> void:
 	population = 0.0
