89baa5d5f7d6de09ef6d9a01744ebf2b938610e5
PowerThePowerfullest64 <andersmb2008@gmail.com>
2026-04-29 15:13:54 +0200

    Added Chunked Loading for Cells and Switched Cells from Being a Class to Arrays

--- map_manager.gd
+++ map_manager.gd
@@ -1,39 +1,75 @@
 extends Node
 
-var cells: Array[Cell] = []
+var population: PackedFloat32Array
 
-const width: int = 64
-const height: int = 64
+const width: int = 512
+const height: int = 512
 
 var cell_count: int # how many cells exist
 
 var tilemaplayer: TileMapLayer
 
+var updating_cells: bool = false
+var cell_index: int = 0
+var cell_updates_per_frame: int = 0
+
+var noise: FastNoiseLite = FastNoiseLite.new()
+
 func initialize() -> void:
+	
 	cell_count = width * height
 	
 	tilemaplayer = get_node("/root/main/TileMapLayer")
 	
+	population = PackedFloat32Array()
+	
+	population.resize(cell_count)
+	
+	noise.seed = randi_range(0, pow(2, 31)-1)
+	noise.frequency = 0.005
+	noise.fractal_octaves = 24
+	
 	for i in range(cell_count):
-		cells.append(Cell.new(i))
+		population[i] = 100.0
 		
 		var x: int = i % width
-		var y: int = int(float(i) / float(width)) # changed it to remove the warning :(
+		var y: int = i / width
 		
 		var pos_2d: Vector2i = Vector2i(x, y)
+		var value: float = (noise.get_noise_2d(x, y) + 1.0) * 0.5
+		var atlas: Vector2i = Vector2i.ZERO if value < 0.45 else Vector2i(1, 0)
 		
-		tilemaplayer.set_cell(pos_2d, 0, Vector2i.ZERO)
+		tilemaplayer.set_cell(pos_2d, 0, atlas)
 		
 	TimeManager.day_passed.connect(tick)
-	
-	
-func update_cells() -> void:
-	for i in range(cell_count):
-		cells[i].update()
+
 
 func _ready() -> void:
 	initialize()
 
+func _process(_delta: float) -> void:
+	if not updating_cells: return
+	
+	if cell_index >= cell_count:
+		updating_cells = false
+		print("Finished Updating Cells")
+		return
+	
+	var cells_left: int = cell_count - cell_index
+	
+	var count: int = min(cell_updates_per_frame, cells_left)
+	var end_index: int = cell_index + count
+	
+	for i in range(cell_index, end_index):
+		population[i] *= 1.005
+	
+	cell_index += end_index
+
 func tick(_day: int) -> void:
-	update_cells()
-	print("Updated Cells!")
+	updating_cells = true
+	cell_index = 0
+	
+	var frames_to_update_cells: int = int(60.0 / TimeManager.tps)
+	cell_updates_per_frame = ceil(cell_count / float(frames_to_update_cells))
+	
+	print("Started Updating Cells")
--- observer_camera.gd
+++ observer_camera.gd
@@ -4,6 +4,9 @@ extends Camera2D
 @export var zoom_sensitivity: float = 1.0
 @export var zoom_rate: float = 10.0
 
+@export var min_zoom: float = 0.1
+@export var max_zoom: float = 10.0
+
 var target_zoom: Vector2
 
 func _ready() -> void:
@@ -24,3 +27,6 @@ func update_zoom() -> void:
 		target_zoom *= 1.0 + (0.1 * zoom_sensitivity)
 	elif Input.is_action_just_pressed("zoom_out"):
 		target_zoom /= 1.0 + (0.1 * zoom_sensitivity)
+	
+	# cap zoom
+	target_zoom = Vector2(max(min(target_zoom.x, max_zoom), min_zoom), max(min(target_zoom.y, max_zoom), min_zoom))
